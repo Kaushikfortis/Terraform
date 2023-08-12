698f6cd17a8cf13faba75dd33e5cc6a963b05ef3
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-08-12 22:40:23 +0530

    Create datasource-ec2.tf

---
+++ Providers/AWS/Modules/aws-module-alb/datasource-ec2.tf
@@ -0,0 +1,37 @@
+
+# Filter to fetch multiple instances : aws_instances
+data "aws_instances" "this" {
+  filter {
+    name   = "tag:${var.lb_target_tags_map["name"]}"
+    values = ["${var.lb_target_tags_map["value"]}"]
+  }
+}
+
+output "data_aws_instance_id" {
+  value       = concat(data.aws_instances.this.*.id, [""])[0]
+  description = "data_aws_instance_id"
+}
+
+output "data_aws_instance_ids" {
+  value       = concat(data.aws_instances.this.*.ids, [""])[0]
+  description = "data_aws_instance_ids"
+}
+
+# Filter to fetch single instance : aws_instance
+
+# data "aws_instance" "this" {
+#   filter {
+#     name   = "tag:${var.lb_target_tags_map["name"]}"
+#     values = ["${var.lb_target_tags_map["value"]}"]
+#   }
+# }
+
+# output "data_aws_instance_id" {
+#   value       = concat(data.aws_instance.this.*.id, [""])[0]
+#   description = "data_aws_instance_id"
+# }
+
+# output "data_aws_instance_private_ip" {
+#   value       = concat(data.aws_instance.this.*.private_ip, [""])[0]
+#   description = "data_aws_instance_private_ip"
+# }
