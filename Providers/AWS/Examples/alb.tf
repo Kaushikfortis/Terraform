40fabf7ad5bf05866d27235439d921fb6440f442
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-15 17:32:00 +0530

    Update alb.tf
    alb with cognitop included

--- Providers/AWS/Examples/alb.tf
+++ Providers/AWS/Examples/alb.tf
@@ -56,3 +56,65 @@ module "alb" {
     Environment = "Test"
   }
 }
+
+
+########### HTTP & HTTPS redirects Cognito Authentication traffic #################
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = "vpc-abcde012"
+  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+  security_groups    = ["sg-edcd9784", "sg-edcd9785"]
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTPS"
+      backend_port     = 443
+      target_type      = "instance"
+    }
+  ]
+
+  https_listeners = [
+    {
+      port                 = 443
+      protocol             = "HTTPS"
+      certificate_arn      = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+      action_type          = "authenticate-cognito"
+      target_group_index   = 0
+      authenticate_cognito = {
+        user_pool_arn       = "arn:aws:cognito-idp::123456789012:userpool/test-pool"
+        user_pool_client_id = "6oRmFiS0JHk="
+        user_pool_domain    = "test-domain-com"
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
+
