a3e296d55fbdcb5a4cf81ff141a8dff417ea566b
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-15 16:50:51 +0530

    Update s3.tf
    ALB/NLB log accesss s3 attached

--- Providers/AWS/Examples/s3.tf
+++ Providers/AWS/Examples/s3.tf
@@ -33,3 +33,22 @@ module "s3_bucket_for_logs" {
 
   attach_elb_log_delivery_policy = true
 }
+
+
+################### Bucket with ALB/NLB Log access poliy attached ###################
+
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true  # Required for ALB logs
+  attach_lb_log_delivery_policy  = true  # Required for ALB/NLB logs
+}
