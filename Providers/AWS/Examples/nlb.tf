55cd5e5933b139f0ff9077a372508928e64c9e1f
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-15 17:34:05 +0530

    Update nlb.tf
    Default NLB

--- Providers/AWS/Examples/nlb.tf
+++ Providers/AWS/Examples/nlb.tf
@@ -1,1 +1,47 @@
+############# NLB with TCP_UDP, TCP, UDP, TCL Listeners #################
 
+module "nlb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-nlb"
+
+  load_balancer_type = "network"
+
+  vpc_id  = "vpc-abcde012"
+  subnets = ["subnet-abcde012", "subnet-bcde012a"]
+
+  access_logs = {
+    bucket = "my-nlb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "TCP"
+      backend_port     = 80
+      target_type      = "ip"
+    }
+  ]
+
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "TLS"
+      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+      target_group_index = 0
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "TCP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
