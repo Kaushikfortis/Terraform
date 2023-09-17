7026e4d236af55c625749fe477894ff14b9fdbb5
Kaushik Pandilwar <52320016+Kaushikfortis@users.noreply.github.com>
2023-09-17 22:30:31 +0530

    Update ecr.tf
    ecr code.

--- Providers/AWS/Examples/ecr.tf
+++ Providers/AWS/Examples/ecr.tf
@@ -1,1 +1,29 @@
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
 
+  repository_name = "private-example"
+
+  repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
